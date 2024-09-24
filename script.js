7e2e68a0f475cc31708003c60bd251eeb5f466ff
Aosaf098 <aosafibad10@gmail.com>
2024-09-25 01:15:58 +0600

    FAQ

--- script.js
+++ script.js
@@ -22,23 +22,30 @@ document.getElementById('noakhali-donate-now-btn').addEventListener('click', fun
     // console.log(createPopup('popup'));
 
     // const historyContent = document.getElementById('history-content')
-    const historyTitle = document.getElementById('history-title')
-    const noakhaliTitle = document.getElementById('noakhali-title').innerText.slice(10)
 
-    document.createElement('div')
-    const historyH3 = document.createElement('h3')
-    const historyH5 = document.createElement('h5')
+    
+    
+    if (noakhaliInput === 0 || getDonationAmount('noakhali-input') > getTotalAmount() ) {
+        // Do Nothing
+    } else {
+        const historyTitle = document.getElementById('history-title')
+        const noakhaliTitle = document.getElementById('noakhali-title').innerText.slice(10)
+    
+        document.createElement('div')
+        const historyH3 = document.createElement('h3')
+        const historyH5 = document.createElement('h5')
+        historyH3.innerText = `${noakhaliInput} Taka is Donated for ${noakhaliTitle}`
+        historyH5.innerText = `${new Date()}`
+        historyH3.style.fontWeight = '700'
+        historyH3.style.margin = '10px auto'
+        historyH5.style.fontWeight = '400'
+        historyH5.style.fontSize = '0.8rem'
+    
+        historyTitle.appendChild(historyH3)
+        historyTitle.appendChild(historyH5)
+    }
 
 
-    historyH3.innerText = `${noakhaliInput} Taka is Donated for ${noakhaliTitle}`
-    historyH5.innerText = `${new Date()}`
-    historyH3.style.fontWeight = '700'
-    historyH3.style.margin = '10px auto'
-    historyH5.style.fontWeight = '400'
-    historyH5.style.fontSize = '0.8rem'
-
-    historyTitle.appendChild(historyH3)
-    historyTitle.appendChild(historyH5)
     // historyDiv.appendChild(historyTitle)
 
 })
@@ -53,23 +60,43 @@ document.getElementById('feni-donate-now-btn').addEventListener('click', functio
 
     donationCalculation(feniInput, 'feni-donation-amount')
 
-    const historyTitle = document.getElementById('history-title')
-    const feniTitle = document.getElementById('feni-title').innerText.slice(10)
+    // const historyTitle = document.getElementById('history-title')
+    // const feniTitle = document.getElementById('feni-title').innerText.slice(10)
 
-    document.createElement('div')
-    const historyH3 = document.createElement('h3')
-    const historyH5 = document.createElement('h5')
+    // document.createElement('div')
+    // const historyH3 = document.createElement('h3')
+    // const historyH5 = document.createElement('h5')
 
 
-    historyH3.innerText = `${feniInput} Taka is Donated for ${feniTitle}`
-    historyH5.innerText = `${new Date()}`
-    historyH3.style.fontWeight = '700'
-    historyH3.style.margin = '10px auto'
-    historyH5.style.fontWeight = '400'
-    historyH5.style.fontSize = '0.8rem'
+    // historyH3.innerText = `${feniInput} Taka is Donated for ${feniTitle}`
+    // historyH5.innerText = `${new Date()}`
+    // historyH3.style.fontWeight = '700'
+    // historyH3.style.margin = '10px auto'
+    // historyH5.style.fontWeight = '400'
+    // historyH5.style.fontSize = '0.8rem'
 
-    historyTitle.appendChild(historyH3)
-    historyTitle.appendChild(historyH5)
+    // historyTitle.appendChild(historyH3)
+    // historyTitle.appendChild(historyH5)
+
+    if (feniInput === 0 || getDonationAmount('feni-input') > getTotalAmount() ) {
+        // Do Nothing
+    } else {
+        const historyTitle = document.getElementById('history-title')
+        const feniTitle = document.getElementById('feni-title').innerText.slice(10)
+    
+        document.createElement('div')
+        const historyH3 = document.createElement('h3')
+        const historyH5 = document.createElement('h5')
+        historyH3.innerText = `${feniInput} Taka is Donated for ${feniTitle}`
+        historyH5.innerText = `${new Date()}`
+        historyH3.style.fontWeight = '700'
+        historyH3.style.margin = '10px auto'
+        historyH5.style.fontWeight = '400'
+        historyH5.style.fontSize = '0.8rem'
+    
+        historyTitle.appendChild(historyH3)
+        historyTitle.appendChild(historyH5)
+    }
 
 })
 
@@ -83,23 +110,25 @@ document.getElementById('quota-donate-now-btn').addEventListener('click', functi
 
     donationCalculation(quotaInput, 'quota-donation-amount')
 
-    const historyTitle = document.getElementById('history-title')
-    const quotaTitle = document.getElementById('quota-title').innerText
-
-    document.createElement('div')
-    const historyH3 = document.createElement('h3')
-    const historyH5 = document.createElement('h5')
-
-
-    historyH3.innerText = `${quotaInput} Taka is Donated for ${quotaTitle}`
-    historyH5.innerText = `${new Date()}`
-    historyH3.style.fontWeight = '700'
-    historyH3.style.margin = '10px auto'
-    historyH5.style.fontWeight = '400'
-    historyH5.style.fontSize = '0.8rem'
-
-    historyTitle.appendChild(historyH3)
-    historyTitle.appendChild(historyH5)
+    if (quotaInput === 0 || getDonationAmount('quota-input') > getTotalAmount() ) {
+        // Do Nothing
+    } else {
+        const historyTitle = document.getElementById('history-title')
+        const quotaTitle = document.getElementById('quota-title').innerText.slice(10)
+    
+        document.createElement('div')
+        const historyH3 = document.createElement('h3')
+        const historyH5 = document.createElement('h5')
+        historyH3.innerText = `${quotaInput} Taka is Donated for ${quotaTitle}`
+        historyH5.innerText = `${new Date()}`
+        historyH3.style.fontWeight = '700'
+        historyH3.style.margin = '10px auto'
+        historyH5.style.fontWeight = '400'
+        historyH5.style.fontSize = '0.8rem'
+    
+        historyTitle.appendChild(historyH3)
+        historyTitle.appendChild(historyH5)
+    }
 
 })
 
